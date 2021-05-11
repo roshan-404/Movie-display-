e91bdbddb54c7c9aecf807c6a8137138160430e3
Roshan Kumar Ojha <roshanboyrko@gmail.com>
2021-05-12 04:13:56 +0530

    Fixed Warning

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Movie from './components/Movie'
 
 const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a682055d92179f832e6e64da02191d1e&page=1"
 
-const IMG_API = "https://image.tmdb.org/t/p/w1280"
 
 const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=a682055d92179f832e6e64da02191d1e&query="
 
